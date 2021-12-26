f47bbe15fbfb37bb4492e73a8dc8f64afd1c35a3
jarbsoncosta <jarbsonfc@gmail.com>
2021-12-25 21:23:44 -0300

    trocando url

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export const api = axios.create({  
 
- //baseURL: 'https://stock-print.herokuapp.com/'
-baseURL: 'http://localhost:3333/'
+baseURL: 'https://stock-print.herokuapp.com/'
+//baseURL: 'http://localhost:3333/'
 
 
 })  
