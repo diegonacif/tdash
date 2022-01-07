07a165d254d422b080b4ecbf7bcb7718be6a1a0a
diegonacif <diegoreisnacif@gmail.com>
2022-01-07 20:38:13 -0300

    estruturando layout em createequipment

--- src/pages/CreateEquipment/index.tsx
+++ src/pages/CreateEquipment/index.tsx
@@ -103,136 +103,91 @@ export const CreateProduct = () => {
 
         <Link to="/dashboard"> <AiOutlineLeft /> </Link>
 
+        <h1>Cadastrar equipamento</h1>
+
         <Form onSubmit={handleCreateNewEquipment} >
 
-          <h2>Cadastrar equipamento</h2>
-
-          <div className="modelRow">
-            <div className="modelo">
-              <TextField
-                required
-                value={description}
-                id="outlined-size-small"
-                placeholder="Description"
-                label="Modelo" size="small"
-                margin="dense"
-                color="success"
-                onChange={event => setDescription(event.target.value)}
-              />
-            </div>
+          <div className="modelo">
+            <TextField
+              required
+              value={description}
+              id="outlined-size-small"
+              placeholder="Description"
+              label="Modelo" size="small"
+              margin="dense"
+              color="success"
+              onChange={event => setDescription(event.target.value)}
+            />
           </div>
 
-          <div className="row2">
-            <div className="tombo">
-              <TextField
-                required
-                type="number"
-                value={patrimony}
-                id="outlined-size-small"
-                placeholder="Patrimônio"
-                label="Patrimônio" size="small"
-                margin="dense"
-                color="success"
-                onChange={event => setPatrimony((event.target.value))}
-              />
-            </div>
-
-
-            <div className="suprimento">
-              <TextField
-                required
-                value={supply}
-                id="outlined-size-small"
-                placeholder="Suprimento"
-                label="Suprimento" size="small"
-                margin="dense"
-                color="success"
-                onChange={event => setSupply(event.target.value)}
-              />
-            </div>
-
+          <div className="tombo">
+            <TextField
+              required
+              type="number"
+              value={patrimony}
+              id="outlined-size-small"
+              placeholder="Patrimônio"
+              label="Patrimônio" size="small"
+              margin="dense"
+              color="success"
+              onChange={event => setPatrimony((event.target.value))}
+            />
           </div>
 
-
-          <div className="row2">
-            <div className="transformado">
-              <TextField
-                type="number"
-                value={transformer}
-                id="outlined-size-small"
-                placeholder="Transformador"
-                label="Transformador" size="small"
-                margin="dense"
-                color="success"
-                onChange={event => setTransformer((event.target.value))}
-              />
-            </div>
-
-            <div className="serie">
-              <TextField
-                required
-                value={serial}
-                id="outlined-size-small"
-                placeholder="N-Serie"
-                label="N-Serie" size="small"
-                margin="dense"
-                color="success"
-                onChange={event => setSerial(event.target.value)}
-              />
-            </div>
+          <div className="suprimento">
+            <TextField
+              required
+              value={supply}
+              id="outlined-size-small"
+              placeholder="Suprimento"
+              label="Suprimento" size="small"
+              margin="dense"
+              color="success"
+              onChange={event => setSupply(event.target.value)}
+            />
           </div>
 
-          <div className="categoria">
-            <select onChange={event => setCategory_id(event.target.value)} value={category_id} required>
-              <option value="">Categoria</option>
-              {categories.map((category) => (
-                <option key={category.name} value={category.id} >{category.name}</option>
-              ))}
-            </select>
+          
+
+          <div className="serie">
+            <TextField
+              required
+              value={serial}
+              id="outlined-size-small"
+              placeholder="N-Serie"
+              label="N-Serie" size="small"
+              margin="dense"
+              color="success"
+              onChange={event => setSerial(event.target.value)}
+            />
           </div>
 
-          <div className="cliente">
-            <select onChange={event => setCustomer_id(event.target.value)} value={customer_id} required>
-              <option value="">Cliente</option>
-              {customers.map((customer) => (
-                <option key={customer.name} value={customer.id}>{customer.name}</option>
-              ))}
-            </select>
+          
+
+          <div className="contador-inicial">
+            <TextField
+              value={count_initial}
+              id="outlined-size-small"
+              placeholder="Contador Inicial"
+              label="Contador Inicial" size="small"
+              margin="dense"
+              color="success"
+              onChange={event => setCount_initial((event.target.value))}
+            />
           </div>
 
-          <div className="row2">
-            <div className="contador-inicial">
-              <TextField
-                value={count_initial}
-                id="outlined-size-small"
-                placeholder="Contador Inicial"
-                label="Contador Inicial" size="small"
-                margin="dense"
-                color="success"
-                onChange={event => setCount_initial((event.target.value))}
-              />
-            </div>
-
-
-
-            <div className="status">
-
-              <div className="status">
-                <select onChange={event => setStatus(event.target.value)} required>
-                  <option value="">Status</option>
-                  <option value={selectStatus.status = "estoque"}>estoque</option>
-                  <option value={selectStatus.status = "cliente"}>cliente</option>
-                  <option value={selectStatus.status = "manutenção"}>manutenção</option>
-                  <option value={selectStatus.status = "aguardando peças"}>aguardando peças</option>
-                  <option value={selectStatus.status = "sem conserto"}>sem conserto</option>
-
-                </select>
-              </div>
-
-
-            </div>
+          <div className="transformado">
+            <TextField
+              type="number"
+              value={transformer}
+              id="outlined-size-small"
+              placeholder="Transformador"
+              label="Transformador" size="small"
+              margin="dense"
+              color="success"
+              onChange={event => setTransformer((event.target.value))}
+            />
           </div>
-
 
           <div className="obs">
             <TextField
@@ -246,10 +201,42 @@ export const CreateProduct = () => {
             />
           </div>
 
+          <div className="status">
+            <select onChange={event => setStatus(event.target.value)} required>
+              <option value="">Status</option>
+              <option value={selectStatus.status = "estoque"}>estoque</option>
+              <option value={selectStatus.status = "cliente"}>cliente</option>
+              <option value={selectStatus.status = "manutenção"}>manutenção</option>
+              <option value={selectStatus.status = "aguardando peças"}>aguardando peças</option>
+              <option value={selectStatus.status = "sem conserto"}>sem conserto</option>
 
-          <button type="submit">
-            Cadastrar
-          </button>
+            </select>
+          </div>
+
+          <div className="cliente">
+            <select onChange={event => setCustomer_id(event.target.value)} value={customer_id} required>
+              <option value="">Cliente</option>
+              {customers.map((customer) => (
+                <option key={customer.name} value={customer.id}>{customer.name}</option>
+              ))}
+            </select>
+          </div>
+
+          <div className="categoria">
+            <select onChange={event => setCategory_id(event.target.value)} value={category_id} required>
+              <option value="">Categoria</option>
+              {categories.map((category) => (
+                <option key={category.name} value={category.id} >{category.name}</option>
+              ))}
+            </select>
+          </div>
+
+          <div className="buttonContainer">
+            <button type="submit">
+              Cadastrar
+            </button>
+          </div>
+
 
 
         </Form>
--- src/pages/CreateEquipment/styles.ts
+++ src/pages/CreateEquipment/styles.ts
@@ -14,83 +14,89 @@ export const Container = styled.div`
   margin: 0 auto;
   padding: 0;
   max-width: 100%;
-
 `;
+
+
 export const Content = styled.div`
+  position: relative;
   max-width: 100%;
   min-width: 700px;
   margin: 0 1rem 0 0;
   padding: 1rem 2rem;
   background-color: var(--shape);
   border-radius: 4px;
+
+  h1 {
+    display: flex;
+    padding-top: 1.5rem;
+    padding-bottom: 1rem;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--background);
+  }
 `;
 
+
 export const Form = styled.div`
-  .modelRow {
-    display: flex;
-    flex-direction: row;
+  display: grid;
+  justify-content: center;
+  padding: 1rem 6rem;
+  height: 89%;
 
-    .modelo {
-      width: 70%;
-      .css-4mzek5-MuiFormControl-root-MuiTextField-root {
-        width: 100%;
-      }
-    }
-  }
+  grid-template-columns: repeat(4, 25%);
+  grid-template-rows: repeat(7, 3.7rem);
+  grid-auto-flow: row;
+  grid-row-gap: 0;
+  grid-column-gap: 1rem;
 
-  .row2 {
-    display: flex;
-    flex-direction: row;
-    gap: 1rem;
+  background-color: var(--background);
 
-    .tombo {
-      width: 50%;
-      .css-4mzek5-MuiFormControl-root-MuiTextField-root {
-        width: 100%;
-      }
-    }
-
-    .suprimento {
-      width: 50%;
-      .css-4mzek5-MuiFormControl-root-MuiTextField-root {
-        width: 100%;
-      }
-    }
-  }
-
-  h2 {
-    color: var(--text-title);
-    font-size: 1.5rem;
-    margin-bottom: 1.5rem;
-  }
-
-  input {
+  /* divs */  
+  .modelo {
     width: 100%;
-    padding: 0 1.5rem;
-    height: 4rem;
-    border-radius: 0.25rem;
-
-    border: 1px solid #d7d7d7;
-    background: #e7e9ee;
-
-    font-weight: 400;
-    font-size: 1rem;
-
-    &::placeholder {
-      color: var(--text-body);
+    grid-column-start: span 2;
+    .css-4mzek5-MuiFormControl-root-MuiTextField-root {
+      width: 100%;
     }
+  }
+  
+  .tombo {
+    width: 100%;
+    .css-4mzek5-MuiFormControl-root-MuiTextField-root {
+      width: 100%;
+    }
+  }
 
-    & + input {
-      margin-top: 1rem;
+  .suprimento {
+    width: 100%;
+    .css-4mzek5-MuiFormControl-root-MuiTextField-root {
+      width: 100%;
+    }
+  }
+
+  .serie {
+    width: 100%;
+    grid-column-start: span 2;
+    .css-4mzek5-MuiFormControl-root-MuiTextField-root {
+      width: 100%;
+    }
+  }
+
+  .obs {
+    width: 100%;
+    grid-column-start: span 4;
+    .css-4mzek5-MuiFormControl-root-MuiTextField-root {
+      width: 100%;
     }
   }
 
   .buttonContainer {
     display: flex;
     justify-content: center;
+    grid-column-start: span 4;
 
     button[type="submit"] {
-      width: 70%;
+      width: 12rem;
       padding: 0 1.5rem;
       height: 4rem;
       background: var(--green);
@@ -104,14 +110,43 @@ export const Form = styled.div`
       margin-right: auto;
   
       transition: filter 0.2s;
-      transition: font-size 0.1s;
   
       &:hover {
         filter: brightness(1.1);
-        font-size: 1.05rem;
       }
     }
   }
+  /* divs end */
+
+  h2 {
+    color: var(--text-title);
+    font-size: 1.5rem;
+    margin-bottom: 1.5rem;
+  }
+
+  input {
+    width: 100%;
+    padding: 0 1.5rem;
+    height: 2.5rem;
+    border-radius: 0.25rem;
+
+    border: 1px solid #d7d7d7;
+    /* background: #e7e9ee; */
+    background-color: var(--shape);
+
+    font-weight: 400;
+    font-size: 1rem;
+
+    &::placeholder {
+      color: var(--text-body);
+    }
+
+    & + input {
+      margin-top: 1rem;
+    }
+  }
+
+  
 `;
 
 
